b879cc7778bdb1c520d1a6dd77562397040b032c
Dorian <dorian.morel-sapene@telecomnancy.net>
2017-05-13 23:37:17 +0200

    And couchDB

--- src/Search/searchOrphadata.java
+++ src/Search/searchOrphadata.java
@@ -15,60 +15,114 @@ public class searchOrphadata {
 	
 	static String symptom = "Abnormal%20colour%20of%20the%20urine";
 	static String symptom2 = "Abnormal%20cry/voice/phonation%20disorder/nasal%20speech";
-	static ArrayList<ArrayList<String>> stockOrpha = new ArrayList<ArrayList<String>>();
-
+	static ArrayList<ArrayList<String[]>> stockOrpha = new ArrayList<ArrayList<String[]>>();
+	
 	
 	public searchOrphadata() throws IOException, ParseException{
 			
         }
-	
-	 public static void affiche() throws IOException, ParseException{
-		 	URL FichierJson = new URL("http://couchdb.telecomnancy.univ-lorraine.fr/orphadatabase/_design/clinicalsigns/_view/GetDiseaseByClinicalSign?key=%22"+symptom2+"%22");
+	 public static ArrayList<String[]> getOrphadataData(String[] args) throws IOException, ParseException{
+		 ArrayList<String[]> results = new ArrayList<String[]>();
+		 for (String arg : args){
+			String trimedArg=arg.trim();
+			String transformedArg=trimedArg.replace(" ", "%20");
+			String key=transformedArg.substring(0,transformedArg.length()-1);
+			URL FichierJson = new URL("http://couchdb.telecomnancy.univ-lorraine.fr/orphadatabase/_design/clinicalsigns/_view/GetDiseaseByClinicalSign?startkey=%22"+key+"%22&endkey=%22"+key+"z%22");
 		    BufferedReader br = new BufferedReader(new InputStreamReader(FichierJson.openStream()));
 		    JSONParser parser = new JSONParser();
 		    Object obj = parser.parse(br);
-	        JSONObject jsonObject = (JSONObject) obj;
-	        
+	        JSONObject jsonObject = (JSONObject) obj;	        
 	        JSONArray rows = ((JSONArray) jsonObject.get("rows"));
-	        int i = 0;
-	        if(rows != null){
-	        	while(i < rows.size()){
-	        		//System.out.println(rows.get(i).toString()); 
-	        		JSONObject elem = (JSONObject) rows.get(i);
-	        		JSONObject value = (JSONObject) elem.get("value");
-	        		JSONObject disease = (JSONObject) value.get("disease");
-	        		String id_desease =(String) disease.get("id");
+	        
+	        
+	        	ArrayList<String[]> tmp_names = new ArrayList<String[]>();
+	        	int i = 0;
+	        	if(rows != null){
+	        		while(i < rows.size()){
+	        			JSONObject elem = (JSONObject) rows.get(i);
+	        			JSONObject value = (JSONObject) elem.get("value");
+	        			JSONObject disease = (JSONObject) value.get("disease");
+	        			String id_disease =(String) disease.get("id");
+	        			JSONObject named = (JSONObject) disease.get("Name");
+	        			String name_disease = (String)  named.get("text");
+	        			String[] array = new String[]{id_disease,name_disease};
+	        			tmp_names.add(array);
 	        		
-	        		JSONObject named = (JSONObject) disease.get("Name");
-	        		String name_desease = (String)  named.get("text");
-	            	
+	        			i ++;
 	        		
-	        		
-	        		ArrayList<String> identifiant = new ArrayList<String>();
-	        		identifiant.add(0, id_desease);
-	        		identifiant.add(1, name_desease);
-	        		stockOrpha.add(i,identifiant);
-	        		
-	        		i ++;
-	        		
-	        		
+	        		}
 	        	}
-	        	
+	        	stockOrpha.add(tmp_names);
 	        }
+		 if(stockOrpha.size() > 1)
+		    {
+			    
+			    int index =0;
+				int tm = stockOrpha.get(0).size();
+				for (int i =1; i< stockOrpha.size();i++){
+					if(stockOrpha.get(i).size() < tm){
+						tm = stockOrpha.get(i).size();
+						index = i;
+					}
+				}
+				
+			    for(int i =0;i<stockOrpha.get(index).size();i++)
+			    {	int cpt = 0;
+				    boolean contains = true;
+			    	String[] orpha = stockOrpha.get(index).get(i);
+			    	while(cpt < stockOrpha.size())
+			    	{
+			    		if(cpt != index)
+			    		{
+				    		if(!Contains(orpha,stockOrpha.get(cpt)))
+				    		{
+				    			contains = false;
+				    			break;
+				    		}
+			    		}
+			    		cpt++;
+			    	}
+			    	
+//			    	boolean doublon=false;
+//			    	for (int j=0; j<results.size();j++){
+//			    		if (results.get(j)[1].equals(orpha[1]))
+//			    			doublon=true;
+//			    			break;
+//			    	}
+			    	
+			    	if(contains /*&& !doublon*/)
+		    			results.add(orpha);
+			    }
+		    }
+		    else
+		    	results = stockOrpha.get(0);
+		 
+		return results;
+		 
+  }
+	 public static boolean Contains(String[] value,ArrayList<String[]> List)
+     {
+         for(String[] val : List)
+         {
+             if(val[0].equals(value[0]) && val[1].equals(value[1]))
+                 return true;
+         }
+         return false;
      }
-	
+	 
+	 public static void affiche(ArrayList<String[]> t ){
+	        for (int i = 0; i < t.size(); i++){
+	            System.out.print(t.get(i)[0]+"\t");
+	            System.out.println(t.get(i)[1]);
+	        }
+	    }
+	 
 	  public static void main (String[] args) throws IOException, ParseException{
-      	affiche();
-      	for (int i= 0; i < stockOrpha.size();i++){
-      		System.out.print( stockOrpha.get(i).get(0) + "   ");
-      		System.out.println( stockOrpha.get(i).get(1));
-      	}
-	}
+		  
+		  ArrayList<String[]> data=getOrphadataData(new String[]{"Micropenis","Delayed dentition"});
 
-
-
-
-    
-   
+      	affiche(data);
+      	System.out.println("Size of the list : " +data.size());
+	}  
 	
 }
